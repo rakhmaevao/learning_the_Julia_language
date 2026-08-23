fb1e0013fc652f335ec0a57182a80ea0e82f6011
Александр Рахмаев <rahmaevao@engee.cloud>
2026-08-23 21:34:31 +0300

    4

--- problems/0004-median-of-two-sorted-arrays/solution.jl
+++ problems/0004-median-of-two-sorted-arrays/solution.jl
@@ -6,19 +6,15 @@ using LeetKit.Support
 function find_median_sorted_arrays(nums1::Vector{Int}, nums2::Vector{Int})::Float64
     result_vector = []
     i_1, i_2 = 1, 1
-    while true
+    while i_1 <= length(nums1) || i_2 <= length(nums2)
         num1 = i_1 <= length(nums1) ? nums1[i_1] : Inf
         num2 = i_2 <= length(nums2) ? nums2[i_2] : Inf
 
-        if isinf(num1) && isinf(num2)
-            break
-        end
-
         if num1 <= num2
-            append!(result_vector, num1)
+            push!(result_vector, num1)
             i_1 = i_1 + 1
         else
-            append!(result_vector, num2)
+            push!(result_vector, num2)
             i_2 = i_2 + 1
         end
     end
@@ -28,7 +24,8 @@ function find_median_sorted_arrays(nums1::Vector{Int}, nums2::Vector{Int})::Floa
     if isodd(len)
         return result_vector[Integer(ceil(len / 2))]
     else
-        return (result_vector[Integer(len / 2)] + result_vector[Integer(len / 2) + 1]) / 2
+        
+        return (result_vector[len ÷ 2] + result_vector[len ÷ 2 + 1]) / 2
     end
 end
 
