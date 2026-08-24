e885fa442b16cb0c16bd8722b43b05c996fba29e
Александр Рахмаев <rahmaevao@engee.cloud>
2026-08-24 08:23:11 +0300

    5

--- problems/0005-longest-palindromic-substring/solution.jl
+++ problems/0005-longest-palindromic-substring/solution.jl
@@ -4,11 +4,30 @@
 using LeetKit.Support
 
 function longest_palindrome(s::String)::AbstractString
-    # TODO: напишите своё решение
-    error("Задача ещё не решена: longest_palindrome")
+    if isempty(s)
+        return ""
+    end
+    max_palindrome_substring = ""
+    for i in eachindex(s)
+        current_substring = "$(s[i])"
+        for j in i + 1:length(s)
+            current_substring = current_substring * s[j]
+            if _is_palindromic(current_substring)
+                if length(current_substring) > length(max_palindrome_substring)
+                    max_palindrome_substring = current_substring
+                end
+            end
+        end
+    end
+    return max_palindrome_substring
+
 end
 
-function _longest_palindrome(s, l, r)
-    # TODO: напишите своё решение
-    error("Задача ещё не решена: _longest_palindrome")
+function _is_palindromic(s:: String)::Bool
+    for i in 1:length(s) ÷ 2
+        if s[i] != s[end-i+1]
+            return false
+        end
+    end
+    return true
 end
--- problems/0005-longest-palindromic-substring/test.jl
+++ problems/0005-longest-palindromic-substring/test.jl
@@ -1,4 +1,6 @@
 @testset "5.longest-palindromic-substring.jl" begin
     @test longest_palindrome("babad") == "bab"
     @test longest_palindrome("cbbd") == "bb"
+    @test longest_palindrome("somepallaplongesttsegnol") == "longesttsegnol"
+    @test longest_palindrome("") == ""
 end
